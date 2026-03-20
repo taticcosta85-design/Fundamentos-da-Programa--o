dc8f7d38a77b5c9722b48ba7f1980cead37bb180
Tatiana Costa <taticcosta85@gmail.com>
2026-03-20 09:42:19 -0300

    Subindo arquivos do dia 20/03

---
+++ README.md
@@ -0,0 +1,23 @@
+# ANOTAÇÕES DE FUNDAMENTOS DA PROGRAMAÇÃO
+
+## Tipos de dados em python
+1.  string
+2.  number int
+3.  number float
+4.  boolean 
+
+## Operadores matemáticos - básicos
++ -> adição
+- -> subtração
+* -> multiplicação
+/ -> divisão
+
+## Métodos em python
+1. print() - > Exibe informações no terminal.
+
+## Format em python 
+
+# Estrutura de repetição
+``if (se)`` - > Verifica se uma condição é true(verdadeira). Se for, ele executa o código.
+``elif (senão se)`` - > é usado para testar várias condições. Ele só executa se todas as condições anteriores forem falsas.  
+``else (senão)`` - > ele executa o código se a condição if for false(falsa). 
---
+++ aula_08/estruturas.py
@@ -0,0 +1,22 @@
+# # Estrutura de repetição
+# # if (se) - > Verifica se uma condição é true(verdadeira). Se for, ele executa o código.
+# # elif (senão se) - > é usado para testar várias condições. Ele só executa se todas as condições anteriores forem falsas.  
+# # else (senão) - > ele executa o código se a condição if for false(falsa). 
+
+# idade_usuario = 15
+# # se o usuário for maior de 18 anos, eu vou passar a informação: Você é maior de idade; senão, Você é menor de idade. 
+# if idade_usuario >= 18:
+#     print("Você é maior de idade") 
+# else: 
+#     print("Você é menor de idade")    
+
+idade = 73
+if idade>= 18:
+    print("Você é maior de idade.")
+elif idade >=0 and idade < 18: 
+    print("Você é jovem de idade.")
+elif idade >=70: 
+    print("Você é experiente de idade.")
+else: 
+    print("Você é menor de idade.") 
+       
