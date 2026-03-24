57b9373d6b485e573a0f790d3311fa36e84f08bd
Tatiana Costa <taticcosta85@gmail.com>
2026-03-24 09:48:07 -0300

    atualização

--- README.md
+++ README.md
@@ -12,8 +12,15 @@
 * -> multiplicação
 / -> divisão
 
+## Operadores lógicos
+
+and - > e - > Se duas condições forem verdadeiras, o resultado é verdadeiro. 
+or - > ou - > Se pelo menos uma condição for verdadeira, o resultado é verdadeiro.
+not - > Ele altera o valor booleano da condição.
+
 ## Métodos em python
 1. print() - > Exibe informações no terminal.
+2. input() - > Capturar uma informação no terminal.
 
 ## Format em python 
 
---
+++ aula_09/estruturas_pizzaria.py
@@ -0,0 +1,12 @@
+# Variaáveis da pizzaria 
+pizza_sabor = input("Informe o sabor da pizza -  [frango com requeijão], [calabresa], [mussarela], [banana com chocolate]: ")
+pizza_tamanho = input("Informe o tamanho da pizza -  [pequena], [média], [grande]: ")
+dia_semana = input("Informe o dia da semana - [quarta], [quinta], [sexta], [sábado], [domingo]: ")
+
+print(f" O sabor escolhido da pizza é {pizza_sabor}, o tamanho é {pizza_tamanho} e hoje é {dia_semana}.")
+# Promoções - > Estruturas condicionais 
+# Comprando qualquer pizza e qualquer tamanho no sábado, o refri é gratuito.
+
+# Comprando uma pizza de calabresa tamanho médio, em qualquer dia, o frete é gratuito.
+
+# Comprando qualquer pizza de qualquer tamanho no domingo, o frete e o refri são gratuitos. 
