923c360a58b5f791a5207fb14ee720bb46f62874
Tatiana Costa <taticcosta85@gmail.com>
2026-04-10 09:06:34 -0300

    avemaria

---
+++ atividade_2/sistema_de_matricula.py
@@ -0,0 +1,22 @@
+#Variáveis do Sistema de Matrícula
+
+idade = int(input("Informe a idade: "))
+nota = float(input("Informe a nota: "))
+frequencia = int(input("Informe a frequência: "))
+
+# A matrícula será aprovada se a idade for >= 18, a nota for >= 6 e a frequencia for > 75%.
+
+print(f"Idade: {idade}, Nota: {nota}, Frequência: {frequencia}")
+
+#Estruturas condicionais: 
+
+if idade <18: 
+    print("Matrícula não aprovada: Idade inferior a 18 anos.")
+
+elif nota >=9:
+    print(" Matricula aprovada automaticamente!")
+
+elif idade >=18 and nota >=6 and frequencia >=75:
+    print(" Matrícula aprovada!")
+else:
+    print(" Matrícula não aprovada.")       
